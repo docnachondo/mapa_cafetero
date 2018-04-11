38701089ee1bd667b1d3a9c67d7c07f2007e0d75
Jose M <jmcunat@syntetyk.com>
2018-04-11 18:17:25 +0200

    Menu según estado login

--- index.php
+++ index.php
@@ -43,7 +43,15 @@ if(isset($_SESSION["usuario"])){
           <div id="titular">Mapa de la Resistencia</div>
           <div id="menu">
             <ul>
+              <? if(isset($usuario)){
+                  if($usuario->getActivo() != null){
+                      ?>
+              <li><a href="javascript:mostrar('#ventana');">Buscar</a></li>
+              <li><a href="funciones/controlador.php?accion=desconectar">Salir</a></li>
+              <? } ?>
+              <? }else{ ?>
               <li><a href="javascript:mostrar('#ventana');">Entrar</a></li>
+            <? } ?>
             </ul>
           </div>
         </div>
@@ -53,8 +61,7 @@ if(isset($_SESSION["usuario"])){
         <? if(isset($usuario)){
             if($usuario->getActivo() != null){
                 ?>
-                <a href="funciones/controlador.php?accion=desconectar">SALIR</a>
-                <div id="ventana" class="flotante"> <!-- style="display:none"> -->
+                <div id="ventana" class="flotante">
                   <div class="formulario">
                     <input type="hidden" id="twitter" value="<?=$usuario->getTwitter()?>"/>
                     <input type="hidden" id="id_oyente" value="<?=$usuario->getIdOyente()?>"/>
