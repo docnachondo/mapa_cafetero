675720cd3134549d2ce215626c18d882a8fb2941
Nacho Escursell <Nacho Escursell@DevelopControl>
2018-04-12 11:35:30 +0200

    Oculta el buscador en Registro

--- header.php
+++ header.php
@@ -3,16 +3,17 @@
   <div id="titular">Mapa de la Resistencia</div>
   <div id="menu">
     <ul>
-      <? if(isset($usuario)){
-          if($usuario->getActivo() != null){
-              ?>
-      <li><a href="javascript:mostrar('#ventana');">Buscar</a></li>
-      <li><a href="funciones/controlador.php?accion=desconectar">Salir</a></li>
-      <? } ?>
-      <? }else{ ?>
-      <li><a href="javascript:mostrar('#ventana2');">Buscar</a></li>
-      <li><a href="javascript:mostrar('#ventana');">Entrar</a></li>
-    <? } ?>
+        <? if(isset($usuario)){
+            if($usuario->getActivo() != null){ ?>
+                <li><a href="javascript:mostrar('#ventana');">Buscar</a></li>
+                <li><a href="funciones/controlador.php?accion=desconectar">Salir</a></li>
+            <? } ?>
+        <? }else{
+            if(!$_SERVER['PHP_SELF'] == "/registro.php"){ ?>
+                <li><a href="javascript:mostrar('#ventana2');">Buscar</a></li>
+            <? } ?>
+        <li><a href="javascript:mostrar('#ventana');">Entrar</a></li>
+        <? } ?>
     </ul>
   </div>
 </div>
